797ab1bbdecc973c266b8737b53351e39cee9579
Cole Gannaway <43042842+cole-gannaway@users.noreply.github.com>
2020-11-26 20:02:10 -0500

    Updates

---
+++ precache-manifest.2c792ee31946e7b06f706e59352e18bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "235a1084ca75aeb07ab9a268c979cfde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "860d53800413356b7fad",
+    "url": "/static/css/main.e0102ece.chunk.css"
+  },
+  {
+    "revision": "7ba900e7bf5c208d5448",
+    "url": "/static/js/2.18d66e98.chunk.js"
+  },
+  {
+    "revision": "9cf9d893b5c51de0aa6dd9e07be64537",
+    "url": "/static/js/2.18d66e98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "860d53800413356b7fad",
+    "url": "/static/js/main.39201b90.chunk.js"
+  },
+  {
+    "revision": "64957af9498ff779d6d8",
+    "url": "/static/js/runtime-main.139d6b28.js"
+  }
+]);
