ab85ef4c4a482ee61a90ec49ae1469ccdc7bb788
Cole Gannaway <43042842+cole-gannaway@users.noreply.github.com>
2020-11-26 20:13:39 -0500

    Updates

---
+++ precache-manifest.b4d9403f9a9e2e0429e589c2ca586db1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b939a3c628bc99a34e326d479db65978",
+    "url": "/CloudGamesFrontEnd/index.html"
+  },
+  {
+    "revision": "c93612d302f38987c75f",
+    "url": "/CloudGamesFrontEnd/static/css/main.e0102ece.chunk.css"
+  },
+  {
+    "revision": "9e071c562c195e4c4e3f",
+    "url": "/CloudGamesFrontEnd/static/js/2.d95b8bd7.chunk.js"
+  },
+  {
+    "revision": "9cf9d893b5c51de0aa6dd9e07be64537",
+    "url": "/CloudGamesFrontEnd/static/js/2.d95b8bd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c93612d302f38987c75f",
+    "url": "/CloudGamesFrontEnd/static/js/main.cee09459.chunk.js"
+  },
+  {
+    "revision": "9ab6cb9524ed98755595",
+    "url": "/CloudGamesFrontEnd/static/js/runtime-main.37caa63d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e2f1e22b33845f11d12b780e94a40a42.js"
+  "/CloudGamesFrontEnd/precache-manifest.b4d9403f9a9e2e0429e589c2ca586db1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CloudGamesFrontEnd/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
